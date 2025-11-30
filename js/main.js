141e54e29395ba2ce892e728f948a80f869828cb
nuhaku <nuhaku404@gmail.com>
2025-11-30 12:11:04 +0900

    再投稿

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// 画面遷移系
+if(document.getElementById("souri_narabikae") != null){
+    document.getElementById("souri_narabikae").addEventListener("click",function(){window.location.href = 'qiizzez/souri_narabikae/souri_narabikae_index.html'})
+}
+
+if(document.getElementById("start_button") != null){
+    document.getElementById("start_button").addEventListener("click",function(){window.location.href = 'souri_narabikae.html'})
+}
+
+if(document.getElementById("ToIndex") != null){
+    document.getElementById("ToIndex").addEventListener("click",function(){window.location.href = 'souri_narabikae_index.html'})
+}
